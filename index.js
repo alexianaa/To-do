1f06af8bffb6cb8dc4e75d75d723e8b475494eda
alexianaa <alexiasilvacardosoo@gmail.com>
2022-10-12 22:56:25 -0300

    conecta database e cria model

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
-const path = require('path')
-const routes = require("./routes/routes")
+const express = require('express');
+const path = require('path');
+const routes = require("./routes/routes");
+const connectToDb = require("./database/db");
 
-const app = express()
-const port = 3000
+connectToDb();
+const app = express();
+const port = 3000;
 
-app.set("view engine", "ejs") // reconhecer arquivo ejs
-app.use(express.static(path.join(__dirname,"public"))) // conectar css com arquivo html
-app.use(routes)
+app.set("view engine", "ejs"); // reconhecer arquivo ejs
+app.use(express.static(path.join(__dirname,"public"))); // conectar css com arquivo html
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`)
